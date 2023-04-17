4f369e9a9fc3c748b70717d61b97d0a614c09460
andyboli <webdevelopmentbolivar@gmail.com>
2023-04-17 13:41:55 -0300

    fix deploy

---
+++ dist/database/config/config.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+const tedious = require("tedious");
+
+dotenv.config();
+
+module.exports = {
+  database: process.env.DB_NAME,
+  dialect: "mssql",
+  dialectOptions: {
+    options: {
+      encrypt: true,
+    },
+  },
+  host: process.env.DB_SERVER,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  dialectModule: tedious,
+};
