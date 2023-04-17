a62ba8f8e44032c926e83434d85f5b973a52f7c8
andyboli <webdevelopmentbolivar@gmail.com>
2023-04-17 12:40:01 -0300

    fix deploY

---
+++ dist/database/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProductsQueries = void 0;
+var queries_1 = require("./queries");
+Object.defineProperty(exports, "ProductsQueries", { enumerable: true, get: function () { return queries_1.ProductsQueries; } });
---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const requests_1 = require("./requests");
+const startApp = () => {
+    requests_1.ExpressService.startExpressApp();
+};
+startApp();
