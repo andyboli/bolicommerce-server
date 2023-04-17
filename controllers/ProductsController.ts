4bb5d78041787b288ceeb9097404e0d3fc22090c
andyboli <webdevelopmentbolivar@gmail.com>
2023-04-17 06:25:13 -0300

    use database on ProductsController

--- controllers/ProductsController.ts
+++ controllers/ProductsController.ts
@@ -1,13 +1,22 @@
 import { RequestHandler } from "express";
+import HapiBoomService from "@hapi/boom";
 
 import { PostProductsParams } from "../entities/Product";
+import { ProductsQueries } from "../database";
 import { PuppeteerService } from "../services";
 
 const deleteProducts: RequestHandler<
   Express.Request,
   Express.Response
 > = async (_request, response) => {
-  console.log("deleteProducts", deleteProducts);
+  try {
+    await ProductsQueries.dropProducts();
+    return response.status(200);
+  } catch (err) {
+    return HapiBoomService.badImplementation(
+      `Error on deleteProducts\n${(err as any)?.name}: ${(err as any)?.message}`
+    );
+  }
 };
 
 const postProducts: RequestHandler<Express.Request, Express.Response> = async (
@@ -15,11 +24,21 @@ const postProducts: RequestHandler<Express.Request, Express.Response> = async (
   response
 ) => {
   const {
-    data: { stored, ...scrapeDomainsParams },
+    data: { stored, ...scrapeParams },
   } = request.body as { data: PostProductsParams };
-  if (!stored) {
-    const products = await PuppeteerService.scrapeProducts(scrapeDomainsParams);
-    return response.status(200).json({ products, stored: false });
+  try {
+    if (!stored) {
+      const products = await PuppeteerService.scrapeProducts(scrapeParams);
+      await ProductsQueries.saveProducts(products);
+      return response.status(200).json({ products, stored: true });
+    } else {
+      const products = await ProductsQueries.findProducts(scrapeParams);
+      return response.status(200).json({ products, stored: true });
+    }
+  } catch (err) {
+    return HapiBoomService.badImplementation(
+      `Error on postProducts\n${(err as any)?.name}: ${(err as any)?.message}`
+    );
   }
 };
 
